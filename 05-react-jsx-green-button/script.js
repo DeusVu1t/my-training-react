d4ea3441995b3457a5a32aa773f0bb04c5098f6f
Yurii Kobets <yuriikobets@gmail.com>
2023-11-22 08:02:25 +0100

    Added react app on 06-create-react for tests

--- 05-react-jsx-green-button/script.js
+++ 05-react-jsx-green-button/script.js
@@ -1,21 +1,21 @@
+const App = ({ initialButtonText, initialClassesList }) => {
+  const [buttonText, setButtonText] = React.useState(initialButtonText);
+  const [classesList, setClassesList] = React.useState(initialClassesList);
 
-const App = () => {
-    console.log('called')
-    let buttonText = 'Click me'
-    const onButtonClick = () => {
-        buttonText = 'hello from react'
-        console.log(buttonText)
-    }
+  const onButtonClick = () => {
+    setButtonText("Hello from react");
+    setClassesList("green-btn");
+  };
 
-    return (
-        <div className="app">
-            <button onClick={onButtonClick}>
-                {buttonText}
-            </button>
-        </div>
-    )
-}
+  return (
+    <div className="app">
+      <button className={classesList} onClick={onButtonClick}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
 
-const container = document.getElementById('app')
-const root = ReactDOM.createRoot(container)
-root.render(<App />)
+const container = document.getElementById("app");
+const root = ReactDOM.createRoot(container);
+root.render(<App initialButtonText="Click me" initialClassesList="" />);
