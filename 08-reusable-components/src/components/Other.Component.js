347a4d774d6abe7801c74b6cc4e24e4ded796e3b
Yurii Kobets <ykobets@leonardo.pl>
2023-11-23 14:39:06 +0100

    Tests for reuse components, props tests, tests return JSX, conditional rendering

---
+++ 08-reusable-components/src/components/Other.Component.js
@@ -0,0 +1,9 @@
+function OtherComponent() {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit animi explicabo repellendus culpa adipisci laborum dolorum voluptates cupiditate repudiandae quia fugit laudantium velit, officia maiores ab corporis quos veritatis? Quasi.</p>
+        </div>
+    )
+}
+
+export default OtherComponent
